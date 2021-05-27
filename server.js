d81fd71b29c1c941f7c1b440128b81dd73431114
Pawan96 <lohana.pawan@gmail.com>
2021-05-27 15:54:43 +1000

    fixing bugs

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-let Express = require('express')
-let app = new Express()
+/** Express Setup */
+const express = require('express')
+const app = express()
+app.use(express.static(__dirname + '/public'))
 
-// server static webpages
-app.use(Express.static('public'))
+// Change the port to 3000 if running locally
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log('Server started, listening on', port))
 
-app.get('/test',(req,res)=>{
+app.get('/test', (req, res) => {
     console.log('Test Endpoint')
     res.send('Test API working')
-})
-
-app.listen(3000,()=>{
-    console.log('Server started on port 3000')
 })
